7b7ebf1acf2274798bdcec714072582820faeff5
Steven Lombardi <lambeauxworks@gmail.com>
2019-08-21 16:06:49 -0700

    First iteration of algorithms for dependency visualization (#2)
    Adds artifact size experiment
    Add feature data structures
    Add generic bundle manipulation
    Adds analysis questions
    Generify functions
    Adds bundle partitioning by name

---
+++ src/main/clojure/deps/analysis.clj
@@ -0,0 +1,171 @@
+(ns deps.analysis
+  "Namespace for composing and rendering data and visualizations."
+  (:require [osgi.core :as osgi]
+            [deps.common :as c]
+            [deps.bundles :as b]
+            [clojure.java.shell :refer [sh]]
+            [dorothy.core :as dot]
+            [dorothy.jvm :refer [save! show!]]))
+
+(def bundle-pars
+  ["spatial-"
+   "catalog-ui"
+   "catalog-validator-"
+   "catalog-transformer-"
+   "catalog-security-"
+   "catalog-rest-"
+   "catalog-plugin-"
+   "catalog-opensearch-"
+   "catalog-core-"
+   "catalog-admin-"
+   "catalog-"
+   "security-sts-"
+   "security-servlet-"
+   "security-rest-"
+   "security-idp-"
+   "security-handler-"
+   "security-"
+   "session-"
+   "platform-"
+   "persistence-"
+   "mime-"
+   "metrics-"
+   "admin-"
+   "action-"
+   "solr-"])
+
+;; ----------------------------------------------------------------------
+;; # Rendering functions / graph support
+;;
+;; Reference for writing graphviz graphs with dorothy and the tools for viewing them.
+
+(def graph-properties
+  {:formats [:png :svg]
+   :layouts [:dot :neato :circo]})
+
+(defn view-by-show
+  "Draws the graph using Java Swing."
+  [graph]
+  (-> (dot/digraph graph)
+      (dot/dot)
+      (show! {:layout :neato})))
+
+(defn view-after-save
+  "Draws the graph by writing a file to /tmp before opening it with the system's
+  default viewing software."
+  ([graph]
+   (view-after-save graph {:format :svg :layout :dot}))
+  ([graph props]
+   (let [file "/tmp/render.svg"]
+     (-> graph dot/dot (save! file props))
+     (sh "open" file))))
+
+(defn layer-create-edge
+  "Used for mapping bundle defs to graphviz data structures."
+  [f bundles]
+  (into [{}
+         (dot/edge-attrs {:color :black})]
+        (c/collmap-collapsed (b/package-depmap f bundles))))
+
+(defn layer-create-node
+  "Used for mapping bundle defs to graphviz data structures."
+  [layer-name layer-label bundles]
+  (dot/subgraph
+    layer-name
+    (into [{:color :blue :label layer-label}
+           (dot/node-attrs {:style :filled})]
+          (map vector bundles))))
+
+(defn layer-bulkcreate-nodes
+  "Used for mapping bundle defs to graphviz data structures."
+  [bundles]
+  (->> bundles
+       (b/partition-bundles-by-id)
+       (map-indexed
+           (fn [idx bundles]
+             (let [cluster (keyword (str "cluster_" idx))]
+               (layer-create-node cluster (str "test_" idx) (map :name bundles)))))))
+
+;; ----------------------------------------------------------------------
+;; # Graphing experiments
+;;
+;; Define, document, and iterate on your graphs here.
+
+(comment
+  "List of DDF bundle names"
+  (->> (osgi/bundles)
+       (filter (b/select-bundles-built-by "lambeaux"))
+       (map :name)))
+
+(comment
+  "List of DDF bundle names partitioned into name groups"
+  (let [pars bundle-pars]
+    (->> (osgi/bundles)
+         (filter (b/select-bundles-built-by "lambeaux"))
+         (b/partition-bundles-by-name pars)
+         (map (fn [coll] (map #(:name %) coll))))))
+
+(comment
+  "Graph of the catalog bundles, limiting dependencies to DDF packages only."
+  (let [bundles (->> (osgi/bundles)
+                     (filter
+                       (b/select-on-all
+                         [(b/select-bundles-built-by "lambeaux")
+                          (b/select-bundles-by-name "catalog-")])))]
+    (view-after-save
+      (dot/digraph
+        (into
+          [(dot/subgraph :edges (layer-create-edge
+                                  b/select-packages-ddf-only bundles))]
+          (layer-bulkcreate-nodes bundles)))
+      {:format :svg :layout :dot})))
+
+(comment
+  "Basic graphviz data sample"
+  (view-after-save [
+                    ; Define the nodes
+                    [:a {}]
+                    [:b]
+                    [:c]
+                    ; Define the edges
+                    [:a :b]
+                    [:a :c]
+                    [:b :c {:arrowhead :empty}]]))
+
+(comment
+  "More advanced graphviz data sample, from http://www.graphviz.org/content/cluster"
+  (view-after-save
+    (dot/digraph
+      [(dot/subgraph
+         :cluster_0 [{:style :filled, :color :lightgrey, :label "platform"}
+                     (dot/node-attrs {:style :filled, :color :white})
+                     [:a0] [:a1] [:a2] [:a3]])
+       (dot/subgraph
+         :cluster_1 [{:color :blue, :label "catalog"}
+                     (dot/node-attrs {:style :filled})
+                     [:b0] [:b3]])
+       (dot/subgraph
+         :cluster_2 [{:color :green, :label "transformer"}
+                     (dot/node-attrs {:style :filled})
+                     [:b1] [:b2] [:b4]])
+       (dot/subgraph
+         :cluster_3 [{:color :purple, :label "feature"}
+                     #_(dot/node-attrs {:style :filled})
+                     [:c1] [:c2] [:c3]])
+       (dot/subgraph
+         :bundleDeps [
+                      {}
+                      (dot/edge-attrs {:color :red})
+                      [:a1 :b3]
+                      [:b2 :a3]
+                      [:a3 :a0]
+                      [:a0 :> :a1 :> :a2 :> :a3]
+                      [:b0 :> :b1]
+                      [:b2 :> :b3]
+                      [:b1 :> :b2]
+                      [:b1 :> :b4]])
+       (dot/subgraph
+         :otherDeps [{}
+                     (dot/edge-attrs {:color :black})
+                     [:b4 :b2]])])
+    {:format :svg :layout :dot}))
--- src/main/clojure/osgi/core.clj
+++ src/main/clojure/osgi/core.clj
@@ -1,76 +1,150 @@
 (ns osgi.core
+  "The osgi.core namespace is all about raw transforms between Java data structures
+  provided by Karaf and the Clojure data structures used throughout the other
+  namespaces. Several key data structures are provided:
+  - Bundle definition
+  - Feature definition"
   (:require [clojure.string :as str]
             [clojure.pprint :refer [pprint print-table]]
             [clojure.reflect :as r])
   (:import [org.osgi.framework FrameworkUtil Bundle]
+           [org.apache.felix.utils.manifest Clause Parser]
            [org.codice.ddf.devtools Nrepl]
            [java.util Collections]))
 
-(defn methods-of [x]
-  (->> (.getMethods (class x) ) (map #(.toGenericString %))))
+(def bundle-states
+  "Enumeration of bundle states."
+  {Bundle/ACTIVE      :active
+   Bundle/INSTALLED   :installed
+   Bundle/RESOLVED    :resolved
+   Bundle/STOPPING    :stopping
+   Bundle/UNINSTALLED :uninstalled
+   Bundle/STARTING    :starting})
 
-(def states {Bundle/ACTIVE      :active
-             Bundle/INSTALLED   :installed
-             Bundle/RESOLVED    :resolved
-             Bundle/STOPPING    :stopping
-             Bundle/UNINSTALLED :uninstalled
-             Bundle/STARTING    :starting})
+(defn- bundle-cxt
+  "Returns the bundle context for the nREPL bundle."
+  []
+  (.. FrameworkUtil (getBundle Nrepl) getBundleContext))
 
-(defn dict->map [dict]
+(defn- methods-of
+  "Attempts to use Java's reflection facilities to get
+  string representations of function signatures. Takes
+  any Java object."
+  [x]
+  (->> (.getMethods (class x))
+       (map #(.toGenericString %))))
+
+(defn- dict->map
+  [dict]
   (into {}
         (map vector
              (map (comp keyword str/lower-case)
                   (Collections/list (. dict keys)))
              (Collections/list (.elements dict)))))
 
-(defn bundle->map [bundle]
+(defn- pair
+  [p]
+  (let [k (keyword (.getName p)) v (.getValue p)]
+    [k
+     (case k
+       (:uses :bundle-classpath :embed-dependency)
+       (str/split v #",")
+       v)]))
+
+(defn- clause->map
+  [clause]
+  [(.getName clause)
+   (into {} (map pair
+                 (concat (.getDirectives clause)
+                         (.getAttributes clause))))])
+
+(defn- parse-header
+  [headers k]
+  (let [header (get headers k)]
+    (assoc headers k (into {} (map clause->map (Parser/parseHeader header))))))
+
+(defn- parse-headers
+  [headers]
+  (-> headers
+      (parse-header :export-package)
+      (parse-header :export-service)
+      (parse-header :import-package)
+      (parse-header :require-capability)
+      (parse-header :embedded-artifacts)))
+
+(defn- bundle->map
+  [bundle]
   {:id       (.getBundleId bundle)
    :name     (.getSymbolicName bundle)
    :version  (.getVersion bundle)
-   :headers  (-> (.getHeaders bundle) dict->map)
+   :headers  (-> (.getHeaders bundle) dict->map parse-headers)
    :location (.getLocation bundle)
-   :state    (get states (.getState bundle) :unknown)
+   :state    (get bundle-states (.getState bundle) :unknown)
    :modified (.getLastModified bundle)
    :fragment (.isFragment bundle)})
 
+; Public operational API
 (defn start! [bundle] (.start bundle))
 (defn stop! [bundle] (.stop bundle))
 (defn uninstall! [bundle] (.uninstall bundle))
+(defn shutdown! [] (-> (bundle-cxt) (. getBundle 0) (. stop)))
 
-(defn bundle-cxt []
-  (.. FrameworkUtil (getBundle Nrepl) getBundleContext))
-
-(defn shutdown! []
-  (-> (bundle-cxt) (. getBundle 0) (. stop)))
-
-(defn bundle-list []
-  (.. (bundle-cxt) getBundles))
-
-(defn bundle-by-location [coor]
-  (->> (bundle-list) (filter #(-> % (.. getLocation) (= coor))) first))
-
+; Services
 (defn get-services [name]
   (let [ctx (bundle-cxt)]
     (->> (. ctx (getAllServiceReferences name nil))
          (map #(. ctx (getService %))))))
 
+; Features
 (defn get-features-service []
   (-> "org.apache.karaf.features.FeaturesService" get-services first))
+(defn list-repositories [] (.listRepositories (get-features-service)))
+(defn list-features [] (->> (list-repositories) (mapcat #(.getFeatures %))))
 
-(defn list-repositories []
-  (.listRepositories (get-features-service)))
+(defn- bundle-list
+  "Retrieves all bundles from the local context."
+  []
+  (.. (bundle-cxt) getBundles))
 
-(defn list-features []
-  (->> (list-repositories) (mapcat #(.getFeatures %))))
+(defn bundles
+  "Get all bundles installed in Karaf."
+  []
+  (->> (bundle-list)
+       (map bundle->map)))
+
+(defn get-cdm-bundle
+  "Fetches the Content Directory Monitor bundle. Useful
+  for examples and testing."
+  []
+  (->> (bundles)
+       (filter #(.contains (:name %) "directorymonitor"))
+       (first)))
+
+(defn get-klojure-bundle
+  "Fetches the Klojure bundle. Useful for examples and
+  testing."
+  []
+  (->> (bundles)
+       (filter #(.contains (:name %) "klojure"))
+       (first)))
 
 (comment
+  "Sample invocation that just lists all the bundles."
+  (bundles))
+
+(comment
+  "Sample invocation that prints a table of resolved bundles."
   (->> (bundle-list)
        (map bundle->map)
        (filter #(-> % :state (= :resolved)))
-       (print-table [:id :state :version :name]))
+       (print-table [:id :state :version :name])))
 
-  (->> (bundle-list) first methods-of)
+(comment
+  "Sample invocation that uses reflection to get valid operations on a bundle."
+  (->> (bundle-list) first methods-of))
 
+(comment
+  "Sample invocation that uses Clojure's bean mapping to datafy features."
   (->> (list-features)
        (filter #(.. % getName (contains "admin-ui")))
        (map bean)))
